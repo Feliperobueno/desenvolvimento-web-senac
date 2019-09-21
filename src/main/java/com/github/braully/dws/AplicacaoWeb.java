9f0cc154bf9d84407def7b410074bfb923624e77
Aluno <Aluno@PC-43693>
2019-09-20 21:52:50 -0300

    Autenticação 3

--- src/main/java/com/github/braully/dws/AplicacaoWeb.java
+++ src/main/java/com/github/braully/dws/AplicacaoWeb.java
@@ -128,7 +128,7 @@ public class AplicacaoWeb extends WebSecurityConfigurerAdapter implements Servle
     public static CustomScopeConfigurer viewScope() {
         CustomScopeConfigurer configurer = new CustomScopeConfigurer();
         configurer.setScopes(
-                Map.of("view", new viewScope()));
+                Map.of("view", new ViewScope()));
         return configurer;
     }
 
